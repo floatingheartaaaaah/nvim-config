1b2ed3bf5497ed2b008ac6ecf88aae7342c26eb5
inthrakumar <inthrak04@gmail.com>
2025-06-13 21:26:07 +0530

    add git plugins for handling merge-conflicts and diff-view

--- lua/ik/lazy/plugins/git.lua
+++ lua/ik/lazy/plugins/git.lua
@@ -87,4 +87,36 @@ return {
 			})
 		end,
 	},
+	{
+		"sindrets/diffview.nvim",
+		config = function()
+			local diff = require("diffview")
+			diff.setup({})
+		end,
+	},
+	{
+		"akinsho/git-conflict.nvim",
+		dependencies = { "yorickpeterse/nvim-pqf" },
+		config = function()
+			local conflict = require("git-conflict")
+			conflict.setup({
+				default_mappings = true, -- disable buffer local mapping created by this plugin
+				default_commands = true, -- disable commands created by this plugin
+				disable_diagnostics = false, -- This will disable the diagnostics in a buffer whilst it is conflicted
+				list_opener = "copen", -- command or function to open the conflicts list
+				highlights = { -- They must have background color, otherwise the default color will be used
+					incoming = "DiffAdd",
+					current = "DiffText",
+				},
+			})
+		end,
+	},
+	{
+		"rbong/vim-flog",
+		lazy = true,
+		cmd = { "Flog", "Flogsplit", "Floggit" },
+		dependencies = {
+			"tpope/vim-fugitive",
+		},
+	},
 }
